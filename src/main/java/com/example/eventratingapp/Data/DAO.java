0243062ecfe3fed9fafd012bdbb4228d53a47d54
decely <danil.ratushnyj@mail.ru>
2023-05-31 15:04:54 +0300

    JavaDoc Fix

--- src/main/java/com/example/eventratingapp/Data/DAO.java
+++ src/main/java/com/example/eventratingapp/Data/DAO.java
@@ -10,15 +10,15 @@ public interface DAO {
     List<EventsInfo> getEventsList(Connection con) throws SQLException;
     void addEvent(EventsInfo eventsInfo, Connection con) throws SQLException;
     void updateEvent(int eventid, Connection con) throws SQLException;
-    void deleteEvent(int eventid, Connection con);
+    void deleteEvent(int eventid, Connection con) throws SQLException;
 
     List<RatingInfo> getRatingList(Connection con) throws SQLException;
     void addRating(RatingInfo ratingInfo, Connection con) throws SQLException;
-    void updateRating (int ratingid, Connection con);
-    void deleteRating (int ratingid, Connection con);
+    void updateRating (int ratingid, Connection con) throws SQLException;
+    void deleteRating (int ratingid, Connection con) throws SQLException;
 
     List<userInfo> getUserList(Connection con) throws SQLException;
-    void addUser(userInfo userinfo, Connection con);
-    void updateUser (int userid, Connection con);
-    void deleteUser (int userid, Connection con);
+    void addUser(userInfo userinfo, Connection con) throws SQLException;
+    void updateUser (int userid, Connection con) throws SQLException;
+    void deleteUser (int userid, Connection con) throws SQLException;
 }
--- src/main/java/com/example/eventratingapp/Data/DAOImpl.java
+++ src/main/java/com/example/eventratingapp/Data/DAOImpl.java
@@ -7,7 +7,21 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Этот класс - имплементация интерфейса дао, он служит для манипуляцией с базой данных PostreSQL
+ *
+ * @author Фролкин
+ *
+ */
+
 public class DAOImpl implements DAO {
+
+    /**
+     * Этот метод получает список мероприятий с помощью запроса в базе данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @return Список мероприятий
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
     public ObservableList<EventsInfo> getEventsList(Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -26,6 +40,12 @@ public class DAOImpl implements DAO {
         return EventsInfos;
     }
 
+    /**
+     * Этод метод добавляет новое мероприятие в базу данных с помощью запроса
+     * @param eventsInfo Информация о мероприятии, которое будет добавлено в базу данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
     public void addEvent(EventsInfo eventsInfo, Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -42,6 +62,12 @@ public class DAOImpl implements DAO {
         stmt.close();
     }
 
+    /**
+     * Этот метод изменяет существующее мероприятие в базе данных с помощью запроса
+     * @param eventid ID мероприятия, которое будет изменено
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
     public void updateEvent(int eventid, Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -55,11 +81,23 @@ public class DAOImpl implements DAO {
         stmt.close();
     }
 
+    /**
+     * Этот метод удаляет существующее мероприятие в базе данных с помощью запроса
+     * @param eventid ID мероприятия, которое будет удалено
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void deleteEvent(int eventid, Connection con) {
-
+    public void deleteEvent(int eventid, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 
+    /**
+     * Этот метод получает список отметок о посещаемости мероприятий с помощью запроса
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     * @return Список отметок о посещаемости
+     */
     @Override
     public ObservableList<RatingInfo> getRatingList(Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -76,6 +114,12 @@ public class DAOImpl implements DAO {
         return ratingInfos;
     }
 
+    /**
+     * Этот метод добавляет отметку о посещении мероприятия в базу данных с помощью запроса
+     * @param ratingInfo Информация о отметке посещения, которая будет добавлена в базу данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
     public void addRating(RatingInfo ratingInfo, Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -90,17 +134,34 @@ public class DAOImpl implements DAO {
         stmt.close();
     }
 
-
+    /**
+     * Этот метод изменяет информацию о отметке о посещении мероприятия в базе данных с помощью запроса
+     * @param ratingid ID отметки о посещении, которая будет изменена в базе данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void updateRating(int ratingid, Connection con) {
-
+    public void updateRating(int ratingid, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 
+    /**
+     * Этот метод удаляет отметку о посещении мероприятия в базе данных с помощью запроса
+     * @param ratingid ID отметки о посещении, которая будет удалена в базе данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void deleteRating(int ratingid, Connection con) {
-
+    public void deleteRating(int ratingid, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 
+    /**
+     * Этот метод получает список пользователей в базе данных с помощью запроса
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     * @return Список пользователей
+     */
     @Override
     public ObservableList<userInfo> getUserList(Connection con) throws SQLException {
         var stmt = con.createStatement();
@@ -117,18 +178,36 @@ public class DAOImpl implements DAO {
         return UserInfos;
     }
 
+    /**
+     * Этот метод добавляет пользователя в базу данных с помощью запроса
+     * @param userinfo Информация о пользователе, который будет добавлен в базу данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void addUser(userInfo userinfo, Connection con) {
-
+    public void addUser(userInfo userinfo, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 
+    /**
+     * Этот метод изменяет существующего пользователя в базе данных с помощью запроса
+     * @param userid ID пользователя, который будет изменён в базе данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void updateUser(int userid, Connection con) {
-
+    public void updateUser(int userid, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 
+    /**
+     * Этот метод удаляет существующего пользователя в базе данных с помощью запроса
+     * @param userid ID пользователя, который будет удален в базе данных
+     * @param con Информация, необходимая для подключения к базе данных
+     * @throws SQLException Вызывает ошибку при проблемах работы с PostgreSQL
+     */
     @Override
-    public void deleteUser(int userid, Connection con) {
-
+    public void deleteUser(int userid, Connection con) throws SQLException {
+        var stmt = con.createStatement();
     }
 }
